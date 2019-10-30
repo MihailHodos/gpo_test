eb85b3414335c18f920197d2472f48face34d3de
Hodos <mhodos@ya.ru>
2019-10-30 18:43:33 +0700

    connection database

--- src/database/database.connection.ts
+++ src/database/database.connection.ts
@@ -4,11 +4,11 @@ import { join } from 'path';
 const parentDir = join(__dirname, '..'); 
 const connectionOpts: ConnectionOptions = 
     { type: 'mysql', 
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || 'remotemysql.com',
     port: Number(process.env.DB_PORT) || 3306, 
-    username: process.env.DB_USERNAME || 'root', 
-    password: process.env.DB_PASSWORD || '', 
-    database: process.env.DB_NAME || 'koa_gpo',
+    username: process.env.DB_USERNAME || 'KUAJoGg7aM', 
+    password: process.env.DB_PASSWORD || 'n1rEEjCJT5', 
+    database: process.env.DB_NAME || 'KUAJoGg7aM',
      entities: [ `${parentDir}/**/*.entity.ts`, ], synchronize: true, }; 
 const connection:Promise<Connection> = createConnection(connectionOpts);
 export default connection;
